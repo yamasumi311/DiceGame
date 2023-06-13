b4d9a69633452cc193591c0b064f4d547882b4d4
yamasumi311 <chim.0.don@gmail.com>
2023-06-13 21:39:42 +1000

    choose defined number of cards randomly

--- make_cards.py
+++ make_cards.py
@@ -3,14 +3,17 @@ import random
 suits = ['Clubs', 'Hearts', 'Diamonds', 'Spades']
 cardno = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
-def make_cards():
-    cards = []  # start with empty list and add cards
-    for s in suits:
-        for i in cardno:
-            s = random.choice(suits)
-            i = random.choice(cardno)
-            cards.append(f'{i} - {s}')
-    return cards
 
-my_cards = make_cards()
-print(my_cards)
+def make_card():
+    s = random.choice(suits)
+    n = random.choice(cardno)
+    card = f'{s} - {n}'
+    return card
+
+
+numberOfCard = int(input('How many cards do you want to select?: '))
+i = 0
+while i < numberOfCard:
+    my_card = make_card()
+    print(my_card)
+    i += 1
