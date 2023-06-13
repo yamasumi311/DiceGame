1f234ca1f123bb72129bdfa23e337e56e3ea21a6
yamasumi311 <chim.0.don@gmail.com>
2023-06-13 21:16:21 +1000

    choose combinations randomly

--- make_cards.py
+++ make_cards.py
@@ -1,3 +1,5 @@
+import random
+
 suits = ['Clubs', 'Hearts', 'Diamonds', 'Spades']
 cardno = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
@@ -5,6 +7,8 @@ def make_cards():
     cards = []  # start with empty list and add cards
     for s in suits:
         for i in cardno:
+            s = random.choice(suits)
+            i = random.choice(cardno)
             cards.append(f'{i} - {s}')
     return cards
 
