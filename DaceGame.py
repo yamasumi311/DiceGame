05642d4193105e076c7747394e03e42d0a68bf7a
yamasumi311 <chim.0.don@gmail.com>
2023-06-14 22:11:40 +1000

    final outcomes

--- DaceGame.py
+++ DaceGame.py
@@ -47,11 +47,11 @@ while len(user_choices) != number_dice:
 # roll again function
 def roll_again(choices, dice_list):
     print('Rolling again...')
-    time.sleep(3)
     for i in range(len(choices)):
         if choices[i] == 'r':
             dice_list[i] = random.randint(1, 6)
     time.sleep(3)
+
 
 # step5 - roll again based on user choices
 roll_again(user_choices, user_rolls)
@@ -60,12 +60,15 @@ print(f'Player new roll: {user_rolls}')
 # computer's turn
 print('Computer is thinking...')
 time.sleep(3)
+
+
 def computer_strategy1(n):
     # create computer choices: roll everything again
     choices = '' # start with an empty list of choices
     for i in range(n):
         choices = choices + 'r'
     return choices
+
 
 def computer_strategy2(n):
     # create computer choices: roll if < 5
@@ -77,6 +80,13 @@ def computer_strategy2(n):
             choices = choices + '-'
     return choices
 
+# step6
+# decide on what choice - using one of the strategy functions
+computer_choices = computer_strategy2(number_dice)
+print(f'Computer Choice: {computer_choices}')
+# Computer rolls again using the choices it made
+roll_again(computer_choices, computer_rolls)
+print(f'Computer new roll: {computer_rolls}')
 
 # final line in code - deciding who wins
 find_winner(computer_rolls, user_rolls)
